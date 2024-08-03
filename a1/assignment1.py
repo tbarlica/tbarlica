2e74ee7e094ad2b827d68144caa96926f0706625
Traian <tbarlica@gmail.com>
2024-08-03 18:10:15 -0400

    updated description

--- a1/assignment1.py
+++ a1/assignment1.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
 '''
-OPS445 Assignment 1 - Fall 2023
+
+
+Author: Traian Barlica
+Description: OPS445 Assignment 1 - Summer 2023
 Program: assignment1.py 
 The python code in this file is original work written by
-"Student Name". No code in this file is copied from any other source
+Traian Barlica. No code in this file is copied from any other source
 except those provided by the course instructor, including any person,
 textbook, or on-line resource. I have not shared this python script
 with anyone or anything except for submission for grading. I understand
 that the Academic Honesty Policy will be enforced and
 violators will be reported and appropriate action will be taken.
-
-Author: Traian Barlica
-Description: <fill this in>
 '''
 
 import sys
@@ -20,7 +20,7 @@ import sys
 def day_of_week(date: str) -> str:
     "Based on the algorithm by Tomohiko Sakamoto"
     day, month, year = (int(x) for x in date.split('/'))
-    days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'] 
+    days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
     offset = {1:0, 2:3, 3:2, 4:5, 5:0, 6:3, 7:5, 8:1, 9:4, 10:6, 11:2, 12:4}
     if month < 3:
         year -= 1
@@ -35,7 +35,7 @@ def mon_max(month:int, year:int) -> int:
     "returns the maximum day for a given month. Includes leap year check"
     ...
 
-def after(date: str) -> str: 
+def after(date: str) -> str:
     '''
     after() -> date for next day in DD/MM/YYYY string format
 
@@ -58,20 +58,20 @@ def after(date: str) -> str:
     lyear = year % 400
     if lyear == 0:
         leap_flag = True  # this is a leap year
-    
+
     mon_dict= {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30,
            7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
     if mon == 2 and leap_flag:
         mon_max = 29
     else:
         mon_max = mon_dict[mon]
-    
+
     if day > mon_max:
         mon += 1
         if mon > 12:
             year += 1
             mon = 1
-        day = 1  # if tmp_day > this month's max, reset to 1 
+        day = 1  # if tmp_day > this month's max, reset to 1
     return f"{day:02}/{mon:02}/{year}"
 
 def before(date: str) -> str:

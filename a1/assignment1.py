34c3ddb820de4b2855cf2a10bb75cf420e2c8827
Traian <tbarlica@gmail.com>
2024-08-03 22:38:36 -0400

    mon_max function completed and tested

--- a1/assignment1.py
+++ a1/assignment1.py
@@ -33,7 +33,12 @@ def leap_year(year: int) -> bool:
 
 def mon_max(month:int, year:int) -> int:
     "returns the maximum day for a given month. Includes leap year check"
-    ...
+    mon_dict= {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30,
+           7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+    if month == 2 and leap_year(year):
+        return 29
+    else:
+        return mon_dict[month]
 
 def after(date: str) -> str:
     '''
